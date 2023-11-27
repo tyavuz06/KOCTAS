ec5e565a2e42754e8b6023156e78b65cf7abf9f2
Yavuz Unlu <yavuz.unlu@olka.com.tr>
2023-11-27 07:39:29 +0300

    İp kısıtmalar için  AspNetCoreRateLimit özelleştirmeleri yapıldı.

--- KOCTAS.API/Program.cs
+++ KOCTAS.API/Program.cs
@@ -1,8 +1,13 @@
+using AspNetCoreRateLimit;
+using KOCTAS.API;
 using KOCTAS.Business;
 using KOCTAS.Business.Core;
 using KOCTAS.Business.Interfaces;
 using KOCTAS.Data.Core;
 using KOCTAS.Data.Interfaces;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,11 +18,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//IP limits
+builder.Services.AddOptions();
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddControllers();
+
 builder.Services.AddSingleton<IMapper, AutoMap>();
 builder.Services.AddSingleton<IMovieBusiness, MovieBusiness>();
 builder.Services.AddSingleton<IMovieDal, MovieDal>();
 var app = builder.Build();
-
+app.UseIpRateLimiting();
 app.UseSwagger();
 app.UseSwaggerUI();
 
@@ -26,5 +43,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+IIpPolicyStore IpPolicy = app.Services.GetRequiredService<IIpPolicyStore>();
+await IpPolicy.SeedAsync();
 app.Run();
